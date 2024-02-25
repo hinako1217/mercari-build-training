439c998e1bc1108a09b890b52bd66e5b49f979bf
須﨑比奈子 <szk17koko@gmail.com>
2024-02-25 21:07:03 +0900

    エラー処理修正

--- go/app/main.go
+++ go/app/main.go
@@ -61,20 +61,20 @@ func addItem(c echo.Context) error {
 	item.Category = c.FormValue("category")
 	imagefile, err := c.FormFile("image")
 	if err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, Response{Message: err.Error()})
 	}
 
 	//画像ファイルを開く
 	src, err := imagefile.Open()
 	if err != nil {
-		return err
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 	defer src.Close()
 
 	//hash化
 	h := sha256.New()
 	if _, err := io.Copy(h, src); err != nil { //srcからhへ中身をコピー
-		return err
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 	str_hash_sha256 := fmt.Sprintf("%x", h.Sum(nil))
 	item.Image = str_hash_sha256 + ".jpg"
@@ -82,11 +82,11 @@ func addItem(c echo.Context) error {
 	//imagesフォルダに画像ファイルを作成
 	dst, err := os.Create(fmt.Sprintf("images/%s", item.Image))
 	if err != nil {
-		return err
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 	defer dst.Close()
 	if _, err = io.Copy(dst, src); err != nil { //srcからdstへ中身をコピー
-		return err
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 
 	// add item to list
@@ -95,14 +95,14 @@ func addItem(c echo.Context) error {
 	//open file  if it doesn't exist, create file
 	jsonfile, err := os.OpenFile("items.json", os.O_WRONLY|os.O_CREATE, 0664)
 	if err != nil {
-		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 	defer jsonfile.Close()
 
 	//encode
 	encoder := json.NewEncoder(jsonfile)
 	if err := encoder.Encode(itemlist); err != nil {
-		log.Fatal(err)
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 
 	c.Logger().Infof("Receive item: %s, %s, %s", item.Name, item.Category, item.Image)
@@ -112,10 +112,10 @@ func addItem(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-func decodeJson() ItemList {
+func decodeJson() (*ItemList, error) {
 	jsonfile, err := os.Open("items.json")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer jsonfile.Close()
 
@@ -123,36 +123,42 @@ func decodeJson() ItemList {
 	var itemlist ItemList
 	decoder := json.NewDecoder(jsonfile)
 	if err := decoder.Decode(&itemlist); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return itemlist
+	return &itemlist, nil
 }
 
 /*
 e.GET("/items". getItemList)
 */
 func getItemList(c echo.Context) error {
-	itemlist := decodeJson()
+	itemlist, err := decodeJson()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
 
-	return c.JSON(http.StatusOK, itemlist)
+	return c.JSON(http.StatusOK, *itemlist)
 }
 
 /*
 e.GET("/items/:id", getItemDetail)
 */
-func getItemDetail(c echo.Context) error {
+func getItemById(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id")) //string to int
 	if err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, Response{Message: err.Error()})
 	}
-	itemlist := decodeJson()
-
-	if id <= 0 || id > len(itemlist.Items) {
-		log.Fatal(err)
+	itemlist, err := decodeJson()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, itemlist.Items[id-1])
+	if id <= 0 || id > len((*itemlist).Items) {
+		return c.JSON(http.StatusBadRequest, Response{Message: err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, (*itemlist).Items[id-1])
 }
 
 /*
@@ -194,7 +200,7 @@ func main() {
 	e.GET("/", root)
 	e.POST("/items", addItem)
 	e.GET("/items", getItemList)
-	e.GET("/items/:id", getItemDetail)
+	e.GET("/items/:id", getItemById)
 	e.GET("/image/:imageFilename", getImg)
 
 	// Start server
